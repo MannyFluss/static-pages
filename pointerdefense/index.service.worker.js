ba4e5581de153ad2a45a471db31c3a651656433b
MannyFluss <manny.fluss@gmail.com>
2026-06-16 22:53:09 -0700

    Deploy PointerDefense web build (2026-06-17T05:53Z)

--- pointerdefense/index.service.worker.js
+++ pointerdefense/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781666457|1994564';
+const CACHE_VERSION = '1781675587|2573052';
 /** @type {string} */
 const CACHE_PREFIX = 'PointerDefense-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
